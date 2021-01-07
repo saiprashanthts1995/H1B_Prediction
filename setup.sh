a9fe0208186a159fa55d0c6bb971450e8e0047f2
Sai Prashanth T S <saiprashanthts@gmail.com>
2021-01-07 17:42:44 +0530

    updated

--- setup.sh
+++ setup.sh
@@ -1,11 +1,14 @@
 mkdir -p ~/.streamlit/
+
 echo "\
 [general]\n\
 email = \"prashanthsp1995@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
+
 echo "\
 [server]\n\
 headless = true\n\
-enableCORS=false\n\
 port = $PORT\n\
+enableCORS = false\n\
+\n\
 " > ~/.streamlit/config.toml
