6f041035dafa6d41ceef5352075413e3e0603e6b
Sai Prashanth T S <saiprashanthts@gmail.com>
2021-01-07 18:14:16 +0530

    updated

--- setup.sh
+++ setup.sh
@@ -1,3 +1,13 @@
-sh mkdir -p ~/.streamlit/
+mkdir -p ~/.streamlit/
 
-echo "\ [server]\n\ headless = true\n\ port = $PORT\n\ enableCORS = false\n\ \n\ " > ~/.streamlit/config.toml
+echo "\
+[general]\n\
+email = \"your-email@domain.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
